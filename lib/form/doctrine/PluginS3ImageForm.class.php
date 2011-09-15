05878236ef7610cb2657a0b76e0b1534f33d75c4
coopers <coopers@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-09-15 04:58:34 -0500

    updated video mime types
    git-svn-id: http://svn.symfony-project.com/plugins/cpAwsPlugin/trunk@33023 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/form/doctrine/PluginS3ImageForm.class.php
+++ lib/form/doctrine/PluginS3ImageForm.class.php
@@ -13,14 +13,25 @@ abstract class PluginS3ImageForm extends BaseS3ImageForm {
   public function configure() {
     parent::configure();
     $this->validatorSchema['original_filename']->setOption('mime_types', 'image_video'); 
-    $this->validatorSchema['original_filename']->setOption('mime_categories', 
-                                                           array('image_video' => array('image/jpeg', 
-                                                                                        'image/pjpeg',
-                                                                                        'image/png',
-                                                                                        'image/x-png',
-                                                                                        'image/gif',
-                                                                                        'video/mpeg',
-                                                                                        'video/quicktime',
-                                                                                        'video/x-msvideo'))); 
+    $this->validatorSchema['original_filename']->setOption('mime_categories', array('image_video' => array(
+      'image/jpeg', 
+      'image/pjpeg',
+      'image/png',
+      'image/x-png',
+      'image/gif',
+      'video/3gpp',       // video mime-types that Amazon support
+      'video/x-msvideo',
+      'video/x-dv',
+      'video/x-flv',
+      'video/vnd.mpegurl',
+      'video/x-m4v',
+      'video/quicktime',
+      'video/x-sgi-movie',
+      'video/mp4',
+      'video/mpeg',
+      'video/ogv',
+      'video/webm',
+      'video/x-ms-wmv'))); 
   }
+  
 }
